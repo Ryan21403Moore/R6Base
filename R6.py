ab33592e62d1894500d4c3c8ded9372f747b736f
Ryan Moore <21403@burnside.school.nz>
2024-05-23 11:39:30 +1200

    more code comments and removing undeeded things.

--- R6.py
+++ R6.py
@@ -8,26 +8,29 @@ db_path = 'SiegeBase.db'
 
 @app.route('/')
 def home():
-    # Connect to the database
+    #  Connect to the database
     with sqlite3.connect(db_path) as conn:
         cursor = conn.cursor()
+        #  Select all of the defender colums from the database.
         cursor.execute("SELECT * FROM Defenders")
         defenders = cursor.fetchall()
+    #  Render the home.html template, allowing the defenders to be displayed onto the homepage.
     return render_template('home.html', defenders=defenders)
 
 
 @app.route('/defender/<int:defender_id>')
 def defender(defender_id):
-    # Connect to the database
+    #  Connect to the database
     with sqlite3.connect(db_path) as conn:
         cursor = conn.cursor()
+        #  Get all of the defenders for the page.
         cursor.execute("SELECT * FROM Defenders WHERE defender_id = ?", (defender_id,))
         defender = cursor.fetchone()
         cursor.execute("SELECT Gadgets.name, Gadgets.description FROM Gadgets JOIN WeaponGadget ON Gadgets.gadget_id = WeaponGadget.gadget_id WHERE WeaponGadget.defender_id = ?", (defender_id,))
         gadgets = cursor.fetchall()
         cursor.execute("SELECT Weapons.name, Weapons.description FROM Weapons JOIN WeaponGadget ON Weapons.weapon_id = WeaponGadget.weapon_id WHERE WeaponGadget.defender_id = ?", (defender_id,))
         weapons = cursor.fetchall()
-    #  render the defender template with the defender, gadgets, and weapons data
+    #  render the defender template with the defender, gadgets, and weapons data to be displayed to the user.
     return render_template('defender.html', defender=defender, gadgets=gadgets, weapons=weapons)
 
 
